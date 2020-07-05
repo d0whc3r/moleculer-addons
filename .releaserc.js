6f58e8f90abe9aa0ced1a535741af19b5d34a6ef
d0whc3r <d0whc3r@gmail.com>
2020-07-06 01:49:19 +0200

    fix(relase): working with auto release

--- .releaserc.js
+++ .releaserc.js
@@ -12,5 +12,11 @@ module.exports = {
       name: 'alpha',
       prerelease: true
     }
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github'
   ]
 };
